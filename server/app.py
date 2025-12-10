b412e0a8af7260818fab08a9c7a937fee4cb28fd
Guillermo <vital_tmv@yahoo.com>
2025-12-09 18:55:38 -0700

    Day1 add websocket backend setup with flask socketio

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api, Resource
 from flask_cors import CORS
+from flask_socketio import SocketIO, emit, join_room, leave_room
 from flask_jwt_extended import JWTManager
 from models import db
 from config import Config
@@ -14,6 +15,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 CORS(app, resources={r"/*": {"origins": "*"}})
+socketio = SocketIO(app, cors_allowed_origins="*")
 jwt = JWTManager(app)
 api = Api(app)
 
@@ -36,8 +38,25 @@ api.add_resource(MessageListResource, '/messages')
 api.add_resource(ConversationResource, '/users/<int:other_user_id>/messages')
 api.add_resource(MessageResource, '/messages/<int:message_id>')
 
+# WebSocket Events
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+    emit('connection_response', {'status': 'Connected to MessageMe!'})
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
+@socketio.on('join')
+def handle_join(data):
+    user_id = data.get('user_id')
+    if user_id:
+        join_room(f'user_{user_id}')
+        print(f'User {user_id} joined their room')
+
 with app.app_context():
     db.create_all()
-
+    
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True, port=5000)
